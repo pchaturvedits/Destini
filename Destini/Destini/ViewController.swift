65e038e957be247a840c71c43bdeea5342def92f
Priyanka Chaturvedi <priyankachaturvedi19@gmail.com>
2019-09-11 10:10:37 -0500

    Added restart button, worked on the logic to display question

--- Destini/Destini/ViewController.swift
+++ Destini/Destini/ViewController.swift
@@ -9,12 +9,80 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var storyIndex:Int = 1
+    @IBOutlet weak var topBtn: UIButton!
+    @IBOutlet weak var bottomBtn: UIButton!
+    @IBOutlet weak var storyLbl: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
+        setup()
+        
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func selectBtn(_ sender: AnyObject) {
+        if sender.tag == 1 && storyIndex == 1{
+            storyLbl.text = story3.story
+            topBtn.isHidden = false
+            bottomBtn.isHidden = false
+            topBtn.setTitle(story3.answerA, for: .normal)
+            bottomBtn.setTitle(story3.answerB, for: .normal)
+            storyIndex = 3
+        }
+        else if sender.tag == 2 && storyIndex == 1 {
+            storyLbl.text = story2.story
+            topBtn.isHidden = false
+            bottomBtn.isHidden = false
+            topBtn.setTitle(story2.answerA, for: .normal)
+            bottomBtn.setTitle(story2.answerB, for: .normal)
+            storyIndex = 2
+        }
+        else if sender.tag == 1 && storyIndex == 3{
+        storyLbl.text = story6.story
+            topBtn.isHidden = true
+            bottomBtn.isHidden = true
+        //topBtn.setTitle(story6.answerA, for: .normal)
+       // bottomBtn.setTitle(story6.answerB, for: .normal)
+        storyIndex = 6
+        }
+            else if sender.tag == 2 && storyIndex == 3 {
+                storyLbl.text = story5.story
+            topBtn.isHidden = true
+            bottomBtn.isHidden = true
+                //topBtn.setTitle(story5.answerA, for: .normal)
+               // bottomBtn.setTitle(story5.answerB, for: .normal)
+                storyIndex = 5
+        }
+       else if sender.tag == 1 && storyIndex == 2{
+        storyLbl.text = story3.story
+            topBtn.isHidden = false
+            bottomBtn.isHidden = false
+        topBtn.setTitle(story3.answerA, for: .normal)
+        bottomBtn.setTitle(story3.answerB, for: .normal)
+        storyIndex = 3
+        }
+            else if sender.tag == 2 && storyIndex == 2{
+                 storyLbl.text = story4.story
+            topBtn.isHidden = true
+            bottomBtn.isHidden = true
+//                 topBtn.setTitle(story4.answerA, for: .normal)
+//                 bottomBtn.setTitle(story4.answerB, for: .normal)
+                 storyIndex = 4
+        }
+        
+    }
 
+    @IBAction func restartBtn(_ sender: Any) {
+        setup()
+        storyIndex = 1
+    }
+    func setup(){
+        topBtn.isHidden = false
+        bottomBtn.isHidden = false
+        storyLbl.text = story1.story
+        topBtn.setTitle(story1.answerA, for: .normal)
+        bottomBtn.setTitle(story1.answerB, for: .normal)
+        
+    }
+   
 }
-
---
+++ Destini/Destini/story.swift
@@ -0,0 +1,35 @@
+//
+//  story.swift
+//  Destini
+//
+//  Created by Manish Chaturvedi on 9/11/19.
+//  Copyright © 2019 Manish Chaturvedi. All rights reserved.
+//
+
+import Foundation
+struct Stories {
+    let story: String
+    let answerA: String
+    let answerB: String
+}
+//var story1 = Stories(story: "1", answerA: "1a", answerB: "1b")
+//var story2 = Stories(story: "2", answerA: "2a", answerB: "2b")
+//var story3 = Stories(story: "3", answerA: "3a", answerB: "3b")
+//var story4 = Stories(story: "4", answerA: "4a", answerB: "4b")
+//var story5 = Stories(story: "5", answerA: "5a", answerB: "5b")
+//var story6 = Stories(story: "6", answerA: "6a", answerB: "6b")
+
+
+
+
+var story1 = Stories(story: "Your car has blown a tire on a winding road in the middle of nowhere with no cell phone reception. You decide to hitchhike. A rusty pickup truck rumbles to a stop next to you. A man with a wide brimmed hat with soulless eyes opens the passenger door for you and asks: \"Need a ride, boy?\".", answerA: "I\'ll hop in. Thanks for the help!", answerB: "Better ask him if he\'s a murderer first.")
+
+var story2 = Stories(story: "He nods slowly, unphased by the question.", answerA: "At least he\'s honest. I\'ll climb in.", answerB: "Wait, I know how to change a tire.")
+
+var story3 = Stories(story: "As you begin to drive, the stranger starts talking about his relationship with his mother. He gets angrier and angrier by the minute. He asks you to open the glovebox. Inside you find a bloody knife, two severed fingers, and a cassette tape of Elton John. He reaches for the glove box.", answerA: "I love Elton John! Hand him the cassette tape.", answerB: "It\'s him or me! You take the knife and stab him.")
+var story4 = Stories(story: "What? Such a cop out! Did you know traffic accidents are the second leading cause of accidental death for most adult age groups?", answerA: " ", answerB: " ")
+var story5 = Stories(story: "As you smash through the guardrail and careen towards the jagged rocks below you reflect on the dubious wisdom of stabbing someone while they are driving a car you are in.", answerA: " ", answerB: " ")
+var story6 = Stories(story:  "You bond with the murderer while crooning verses of \"Can you feel the love tonight\". He drops you off at the next town. Before you go he asks you if you know any good places to dump bodies. You reply: \"Try the pier.\"", answerA: " ", answerB: " ")
+
+
+
